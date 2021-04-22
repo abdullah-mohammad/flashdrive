928490ceefc6218ab323b737a319644e25481d26
Venance Konan <venancekonan5@gmail.com>
2021-04-22 23:55:23 +0200

    typo

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Voll Nice!!!!! 😻</h1>
+        <h1>Nice!!!!! 😻</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
