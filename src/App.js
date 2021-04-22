fd6c9b7e16046f6784f8713f75c80a46e500d69f
Venance Konan <venancekonan5@gmail.com>
2021-04-23 01:23:18 +0200

    deploy4 image cloudbuild.yaml

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Continuous Nice!!!!! 😻</h1>
+        <h1>Welcome to FlashDrive 😻</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
