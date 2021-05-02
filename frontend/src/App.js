d1af57fd64d61020c7a3d6a486db0d36d24e1fe1
Venance Konan <venancekonan5@gmail.com>
2021-05-02 22:52:16 +0200

    update containers structure 12

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
         <h1>Welcome to FlashDrive 😻</h1>
         <p>Tschüss!!!!!!!!!!!!!</p>
         <p>
-          Halllo hier
+          Halllo
         </p>
         <a
           className="App-link"
