97bb8e6a06fd5c7931fc5cfea6579abcb1473ff9
Venance Konan <venancekonan5@gmail.com>
2021-05-03 00:08:43 +0200

    update base containers structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
         <h1>Welcome to FlashDrive 😻</h1>
         <p>Tschüss!!!!!!!!!!!!!</p>
         <p>
-          Halllo. Nervig! Klappt es ?
+          NOOOOOOOOOOOOOOO
         </p>
         <a
           className="App-link"
