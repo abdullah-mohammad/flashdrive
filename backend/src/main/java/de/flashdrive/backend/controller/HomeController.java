463364b0d5e8c74c48ded9d4c854df5bdc252242
Venance Konan <venancekonan5@gmail.com>
2021-05-06 00:42:45 +0200

    test ingressNginx 9

--- backend/src/main/java/de/flashdrive/backend/controller/HomeController.java
+++ backend/src/main/java/de/flashdrive/backend/controller/HomeController.java
@@ -6,6 +6,7 @@ import com.google.common.collect.ImmutableList;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,17 +23,17 @@ import java.util.Map;
 
 @RestController
 public class HomeController {
-    /*@Autowired
+    @Autowired
     JdbcTemplate jdbcTemplate;
 
-    @GetMapping("/")
+    @GetMapping("/api")
     public List<Map<String, Object>> listUsers() throws IOException {
         return jdbcTemplate.queryForList("SELECT * FROM Users;");
-    }*/
+    }
 
-   @GetMapping("/api")
+   /*@GetMapping("/api")
     public String index() {
        return "Hallo Leute !";
-   }
+   }*/
 
 }
