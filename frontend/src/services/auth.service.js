6d7eebfa8047890718a9deca5c9c73f1ec7e9c87
Venance Konan <venancekonan5@gmail.com>
2021-05-06 23:36:21 +0200

    go live 3

--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_URL = "http://34.95.118.127/api/auth/";
+//const API_URL = "http://localhost:8080/api/auth/";
+//const API_URL = "http://34.95.118.127/api/auth/";
+const API_URL = "http://flashdrive-cloud.de/api/auth/";
 
 class AuthService {
   login(username, password) {
