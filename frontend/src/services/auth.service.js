5a068b0a499e12f3d0ee313e9482d9ee44dbe475
Venance Konan <venancekonan5@gmail.com>
2021-05-06 23:18:28 +0200

    go live 2

--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/auth/";
+const API_URL = "http://34.95.118.127/api/auth/";
 
 class AuthService {
   login(username, password) {
