4b94583b345e745a68269f0fa7dc8ed2bd701887
Al Mohammad, Abdullah <abdullah.almohammad@haw-hamburg.de>
2021-05-06 02:43:25 +0200

    signup backend is done login backend is done

---
+++ backend/src/main/java/de/flashdrive/backend/security/SignupRequest.java
@@ -0,0 +1,87 @@
+package de.flashdrive.backend.security;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class SignupRequest {
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String username;
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String firstname;
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String lastname;
+
+    @NotBlank
+    private String gender;
+
+    @NotBlank
+    private String address;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
