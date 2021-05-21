1406f1b3772f0cfd73276c49df203c00d222e3ea
Venance Konan <venancekonan5@gmail.com>
2021-05-21 14:05:06 +0200

    update API_URL for Production

--- frontend/src/services/file.service.js
+++ frontend/src/services/file.service.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'http://localhost:8080/api/';
+//const API_URL = 'http://localhost:8080/api/';
+const API_URL = 'http://34.117.49.181/api/';
 
 class FileService {
     /* get(id) {
